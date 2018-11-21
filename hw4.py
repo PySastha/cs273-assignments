096be99447fc93728a4ee7fffa4fd21bcba1036f
Sastha Kanagasabai Palaniappan <sastha1995@gmail.com>
2018-11-20 20:41:41 -0800

    3

---
+++ hw4.py
@@ -0,0 +1,88 @@
+# CS273A - HOMEWORK 4
+# ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+import numpy as np
+import mltools as ml
+import matplotlib.pyplot as plt
+import texttable as tt
+# ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+# Statement of Collaboration
+print("Statement of Collaboration:")
+print("\tI have solved this assignment without any collaboration with fellow classmates")
+print("\tVisited blogs, websites for python syntax, coding, library info\n")
+# ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+def find_stat_tabular(inp):
+    list_min = []
+    list_max = []
+    list_mean = []
+    list_var = []
+
+    for i in range(14):
+        list_min.append(np.amin(inp[:, [i]]))
+        list_max.append(np.amax(inp[:, [i]]))
+        list_mean.append(np.mean(inp[:, [i]]))
+        list_var.append(np.var(inp[:, [i]]))
+
+    tab = tt.Texttable()
+    headings = ['Feature', 'Min', 'Max', 'Mean', 'Var']
+    tab.header(headings)
+
+    for i in zip(range(1, 15), list_min, list_max, list_mean, list_var):
+        tab.add_row(i)
+    print(tab.draw())
+# ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+# Loading data:
+X = np.genfromtxt('data/X_train.txt', delimiter=None)
+Y = np.genfromtxt('data/Y_train.txt', delimiter=None)
+X,Y = ml.shuffleData(X,Y)
+
+print("Q-1 Solution:\n-------------\n")
+print("Statistics before Scaling")
+#find_stat_tabular(X)
+
+Xtr, Xva, Ytr, Yva = ml.splitData(X, Y)
+Xt, Yt = Xtr[:5000], Ytr[:5000] # subsample for efficiency (you can go higher)
+XtS, params = ml.rescale(Xt) # Normalized XtS
+XvS, _ = ml.rescale(Xva,params) # Normalized XvS
+
+print("Statistics after Scaling")
+#find_stat_tabular(XtS)
+# ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+def linear_auc_plot(xt,xv,yt,yv):
+    list_tr = []
+    list_va = []
+    r = [-10, -2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2.0, 2.5, 3, 3.5, 4, 4.5, 5, 10]
+
+    for i in r:
+        learner = ml.linearC.linearClassify()
+        learner.train(xt, yt, reg=i, initStep=0.5, stopTol=1e-6, stopIter=100)
+        print("running", i)
+
+        temp1 = learner.auc(xt, yt)
+        list_tr.append(temp1)
+        temp2 = learner.auc(xv, yv)
+        list_va.append(temp2)
+
+    plt.plot(r, list_tr)
+    plt.plot(r, list_va)
+    plt.show()
+# ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+print("Q-2 Solution:\n-------------\n")
+print("AUC value is the area under ROC curve")
+print("Initially, reg was swept from -10 to +10. \nSince it is changing only between -2 to +5, that area has small step size")
+
+# linear_auc_plot(XtS,XvS,Yt,Yva)
+
+XtS_d2 = ml.transforms.fpoly(Xt,2,False) # Degree 2
+XtS_d2,params = ml.rescale(XtS_d2)
+XvS_d2 = ml.transforms.fpoly(XvS,2,False) # Degree 2
+XvS_d2, _ = ml.rescale(XvS_d2,params) # Normalize the features
+
+print("\n", XtS_d2) # To have a idea about degree 2 poly
+print(XtS_d2.shape)
+
+#linear_auc_plot(XtS_d2,XvS_d2,Yt,Yva)
+# ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+print("Q-3 Solution:\n-------------\n")
+
+print ("abc")
+
