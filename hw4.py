fb7e4fbc5aad47477d574cc0eeb5a6b50880d4a7
Sastha Kanagasabai Palaniappan <sastha1995@gmail.com>
2018-11-21 21:05:05 -0800

    9

--- hw4.py
+++ hw4.py
@@ -5,11 +5,6 @@ import mltools as ml
 import matplotlib.pyplot as plt
 import texttable as tt
 # ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
-# Statement of Collaboration
-print("Statement of Collaboration:")
-print("\tI have solved this assignment without any collaboration with fellow classmates")
-print("\tVisited blogs, websites for python syntax, coding, library info\n")
-# ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
 def find_stat_tabular(inp):
     list_min = []
     list_max = []
@@ -35,8 +30,8 @@ X = np.genfromtxt('hw4_data/X_train.txt', delimiter=None)
 Y = np.genfromtxt('hw4_data/Y_train.txt', delimiter=None)
 X,Y = ml.shuffleData(X,Y)
 
-print("\nQ-1 Solution:\n-------------\n")
-print("Statistics before Scaling")
+#print("\nQ-1 Solution:\n-------------\n")
+#print("Statistics before Scaling")
 #find_stat_tabular(X)
 
 Xtr, Xva, Ytr, Yva = ml.splitData(X, Y)
@@ -44,42 +39,42 @@ Xt, Yt = Xtr[:5000], Ytr[:5000] # subsample for efficiency (you can go higher)
 XtS, params = ml.rescale(Xt) # Normalized XtS
 XvS, _ = ml.rescale(Xva,params) # Normalized XvS
 
-print("Statistics after Scaling")
+#print("Statistics after Scaling")
 #find_stat_tabular(XtS)
 # ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
 def linear_auc_plot(xt,xv,yt,yv):
     list_tr = []
     list_va = []
-    r = [-10, -2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2.0, 2.5, 3, 3.5, 4, 4.5, 5, 10]
-
+    #r = [-10, -2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2.0, 2.5, 3, 3.5, 4, 4.5, 5, 10]
+    r = [0,1,2,3,4]
     for i in r:
         learner = ml.linearC.linearClassify()
         learner.train(xt, yt, reg=i, initStep=0.5, stopTol=1e-6, stopIter=100)
-        print("running", i)
+        #print("running", i)
 
         temp1 = learner.auc(xt, yt)
         list_tr.append(temp1)
         temp2 = learner.auc(xv, yv)
         list_va.append(temp2)
 
-    plt.plot(r, list_tr)
-    plt.plot(r, list_va)
+    plt.plot(r, list_tr, label="tr")
+    plt.plot(r, list_va, label="va)")
+    plt.legend(loc='upper left')
     plt.show()
 # ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
 print("\nQ-2 Solution:\n-------------\n")
-print("AUC value is the area under ROC curve")
-print("Initially, reg was swept from -10 to +10. \nSince it is changing only between -2 to +5, that area has small step size")
+#print("AUC value is the area under ROC curve")
+#print("Initially, reg was swept from -10 to +10. \nSince it is changing only between -2 to +5, that area has small step size")
 
-#linear_auc_plot(XtS,XvS,Yt,Yva)
+linear_auc_plot(XtS,XvS,Yt,Yva)
 
-#XtS_d2 = ml.transforms.fpoly(Xt,2,False) # Degree 2
-#XtS_d2,params = ml.rescale(XtS_d2)
-#XvS_d2 = ml.transforms.fpoly(Xva,2,False) # Degree 2
-#XvS_d2, _ = ml.rescale(XvS_d2,params) # Normalize the features
+XtS_d2 = ml.transforms.fpoly(Xt,2,False) # Degree 2
+XtS_d2,params = ml.rescale(XtS_d2)
+XvS_d2 = ml.transforms.fpoly(Xva,2,False) # Degree 2
+XvS_d2, _ = ml.rescale(XvS_d2,params) # Normalize the features
 
-#print("\n", XtS_d2) # To have a idea about degree 2 poly
-#print(XtS_d2.shape)
-#print("It contains all combinations (2nd degree) --> 14 features, their squares and combinations of 2 features")
+print("\n", XtS_d2) # To have a idea about degree 2 poly
+print(XtS_d2.shape)
 
 #linear_auc_plot(XtS_d2,XvS_d2,Yt,Yva)
 # ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
@@ -191,7 +186,7 @@ print("\nQ-4 Solution:\n-------------\n")
 
 #dtree_auc_plot(Xt, Xva, Yt, Yva)
 
-run_dt = 1
+run_dt = 0
 if run_dt:
     K = [1,2,3,4,5,6,7,8,9,10,15,20,25,30,35]  # Or something else
     A = [1,2,3,4,5,6,7,8,9,10,15,20,25,30,35]  # Or something else
